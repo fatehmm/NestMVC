6f281c0f76d1f59c2c659c95c7fd7b45c0c55c05
Fateh Mammadli <fatehmmmdli@Fatehs-MacBook-Air.local>
2022-09-20 20:30:37 +0400

    Project Created

---
+++ NestProject/Controllers/ShopController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NestProject.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+        public IActionResult Product()
+        {
+            return View();
+        }
+
+        public IActionResult Compare()
+        {
+            return View();
+        }
+
+    }
+}
+
---
+++ NestProject/DAL/NestContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace NestProject.DAL
+{
+	public class NestContext : DbContext
+	{
+		public NestContext(DbContextOptions<NestContext> options) :base(options)
+		{
+
+		}
+
+
+
+	}
+}
+
